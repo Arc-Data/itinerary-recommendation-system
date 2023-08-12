f216a06b44d3d723d1d4fd8f0112492d1b22d0ae
Arc-Data <arclight04@gmail.com>
2023-08-12 14:49:38 +0800

    migrations to mysql

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'IRSDB',
+        'USER': 'irsadmin',
+        'PASSWORD': 'irspass',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
