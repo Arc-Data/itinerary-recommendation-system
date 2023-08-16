59a69fa4997739f362aedbdb1376d5ffbb131ed8
Arc-Data <arclight04@gmail.com>
2023-08-16 12:15:25 +0800

    registered to admin

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Location)
+admin.site.register(Spot)
+admin.site.register(Accomodation)
+admin.site.register(FoodPlace)
+admin.site.register(Activity)
+admin.site.register(Itinerary)
+admin.site.register(Day)
+admin.site.register(ItineraryItem)
+admin.site.register(Review)
