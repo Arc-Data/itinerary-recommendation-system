35c68a0b3be17f5311dcdc7f21243814052f41ea
Arc-Data <arclight04@gmail.com>
2023-08-18 16:21:11 +0800

    serializers

--- project/api/urls.py
+++ project/api/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('popular/', popular_spots),
+    path('location/', location),
+    path('spot/<int:pk>/', spot),
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -11,5 +11,19 @@ from .serializers import *
 def popular_spots(request):
     if request.method == "GET":
         spot = Spot.objects.all()[:6]
-        serializer = SpotSerializers(spot, many=True)
+        serializer = SpotPopularSerializers(spot, many=True)
+        return Response(serializer.data)
+
+@api_view(["GET"])
+def location(request):
+    if request.method == "GET":
+        location = Location.objects.all()
+        serializer = LocationSerializers(location, many=True)
+        return Response(serializer.data)
+    
+@api_view(["GET"])
+def spot(request, pk):
+    if request.method == "GET":
+        spot = get_object_or_404(Spot, id=pk)
+        serializer = SpotDetailSerializers(spot)
         return Response(serializer.data)
