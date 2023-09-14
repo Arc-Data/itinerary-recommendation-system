120ceb0382ea9d0cbe342f4d87d051498dd2cb08
Arc-Data <arclight04@gmail.com>
2023-09-14 10:32:53 +0800

    optimized time string related code

--- project/api/management/commands/import_spots.py
+++ project/api/management/commands/import_spots.py
@@ -1,13 +1,20 @@
 import os
 import csv
 from django.conf import settings
-from datetime import timedelta, time
+from datetime import time
 from django.core.management.base import BaseCommand
 from api.models import Spot, Tag
 
 class Command(BaseCommand):
     help = 'Import data from CSV to Spot model'
 
+    def get_time_str(time_str):
+        if not time_str:
+            return None
+        
+        time_str_parts = time_str.split(":")
+        return time(int(time_str_parts[0]), int(time_str_parts[1]), int(time_str_parts[2]))
+        
     def handle(self, *args, **options):
         csv_file = os.path.join(settings.BASE_DIR, 'Spot.csv')
 
@@ -19,31 +26,22 @@ class Command(BaseCommand):
                 opening_time_str = row.get('OpeningTime', '00:00:00')
                 closing_time_str = row.get('ClosingTime', '00:00:00')
                 
-                if opening_time_str:
-                    opening_time_parts = opening_time_str.split(':')
-                    opening_time = time(int(opening_time_parts[0]), int(opening_time_parts[1]), int(opening_time_parts[2]))
-                else:
-                    opening_time = None
-
-                if closing_time_str:
-                    closing_time_parts = closing_time_str.split(':')
-                    closing_time = time(int(closing_time_parts[0]), int(closing_time_parts[1]), int(closing_time_parts[2]))
-                else:
-                    closing_time = None
+                opening_time = self.get_time_str(opening_time_str)
+                closing_time = self.get_time_str(closing_time_str)
 
                 spot = Spot(
                     name=row['Name'],
                     address=row['Address'],
+                    is_closed=is_closed,
                     latitude=float(row['Latitude']),
                     longitude=float(row['Longitude']),
-                    fees=0,
-                    is_closed=is_closed,
+                    fees=row['Fees'] if row['Fees'] != '' else None,
                     opening_time=opening_time,
                     closing_time=closing_time 
                 )
                 spot.save()
 
-                tags = []  # Create a list to store tags
+                tags = []  
                 tag_names = ['Historical', 'Nature', 'Religious', 'Art', 'Activities', 'Entertainment', 'Culture']
 
                 for tag_name in tag_names:
@@ -52,6 +50,6 @@ class Command(BaseCommand):
                         tag, created = Tag.objects.get_or_create(name=tag_name)
                         tags.append(tag)
 
-                spot.tags.set(tags)  # Associate tags with the Spot instance
+                spot.tags.set(tags) 
 
         self.stdout.write(self.style.SUCCESS('Data imported successfully'))
