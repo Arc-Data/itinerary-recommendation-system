d55fad3698d94c52e2fd60f772b9c556a125b83f
Arc-Data <arclight04@gmail.com>
2023-09-18 16:12:57 +0800

    home banners

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,11 @@
-import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
-import Footer from "../components/Footer"
-
 const HomePage = () => {
-	const { user } = useContext(AuthContext)
 	return (
 		<div className = "home--page-content">
-			Home Page
-			{/* <div className="home--page-header">
-				<p className="home--your-trips">Your trips</p>
-				<button className="home--create-trip">New Trip</button>
+			<div className="home--banner">
+				<div className="home--banner-itinerary">You have not created an itinerary yet.</div>
+				<div className="home--banner-business">Promote your food business with CebuRoute</div>
+				<div className="home--banner-ai">Try our AI Recommendations Feature while building your itinerary.</div>
 			</div>
-			<div className="home--trips-container">
-				<p className="home--no-trips">You have no trips yet.</p>
-			</div> */}
 		</div>
 	)
 }
