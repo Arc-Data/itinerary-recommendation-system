e66419a0b37cf5f97e709e83d70ca56498e6310d
Arc-Data <arclight04@gmail.com>
2023-09-18 16:51:41 +0800

    fetch itineraries

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,44 @@
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+
 const HomePage = () => {
+	const { authTokens } = useContext(AuthContext)
+	const [trips, setTrips] = useState(null)
+
+	useEffect(() => {
+		const access = String(authTokens.access)
+		const getUserItineraries = async () => {
+			const response = await fetch('http://127.0.0.1:8000/api/itineraries/', {
+ 				method: "GET",
+				headers: {
+					'Content-Type': 'application.json',
+					'Authorization': `Bearer ${access}`,
+				}
+		 	})
+
+			if(!response.ok) {
+				throw new Error('Error retrieving itinerary information')
+			}
+
+			const data = await response.json()
+			setTrips(data)
+		}
+
+		getUserItineraries();
+	}, [])
+
 	return (
 		<div className = "home--page-content">
-			<div className="home--banner">
+			<header className="home--banner">
 				<div className="home--banner-itinerary">You have not created an itinerary yet.</div>
 				<div className="home--banner-business">Promote your food business with CebuRoute</div>
 				<div className="home--banner-ai">Try our AI Recommendations Feature while building your itinerary.</div>
+			</header>
+			{ trips !== null && (
+			<div>
+				<p className="home--your-trips">Your trips</p>
 			</div>
+			)}
 		</div>
 	)
 }
