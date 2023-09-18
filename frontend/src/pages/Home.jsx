6cf1df8694cd4308664840580154d1ea61d74fe4
Arc-Data <arclight04@gmail.com>
2023-09-18 15:16:18 +0800

    navigation design fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,17 +5,16 @@ import Footer from "../components/Footer"
 const HomePage = () => {
 	const { user } = useContext(AuthContext)
 	return (
-		<>
 		<div className = "home--page-content">
-			<div className="home--page-header">
+			Home Page
+			{/* <div className="home--page-header">
 				<p className="home--your-trips">Your trips</p>
 				<button className="home--create-trip">New Trip</button>
 			</div>
 			<div className="home--trips-container">
 				<p className="home--no-trips">You have no trips yet.</p>
-			</div>
+			</div> */}
 		</div>
-		</>
 	)
 }
 
