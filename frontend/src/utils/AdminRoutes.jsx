700f92a54a8f71bfa1b874ba922b9e1bc76dfa31
Arc-Data <arclight04@gmail.com>
2023-10-27 15:25:57 +0800

    adjusted admin routes

---
+++ frontend/src/utils/AdminRoutes.jsx
@@ -0,0 +1,32 @@
+import { useContext, useEffect } from "react"
+import AuthContext from "../context/AuthContext"
+import { Outlet, useNavigate } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
+
+
+const AdminRoutes = () => {
+    const { user } = useContext(AuthContext)    
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        if(!user) {
+            navigate('/')
+        } 
+    
+        else if(!user.is_staff) {
+            navigate('/home')
+        }
+
+    })
+
+    return (
+        <div className="site-wrapper">
+            <Sidebar />
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
+}
+
+export default AdminRoutes
