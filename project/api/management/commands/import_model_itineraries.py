d198008d282348c7237cc78253fcfffd4de74241
Arc-Data <arclight04@gmail.com>
2023-10-29 18:48:54 +0800

    temporary csv fixing

---
+++ project/api/management/commands/import_model_itineraries.py
@@ -0,0 +1,33 @@
+import os, csv
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from api.models import Spot, ModelItinerary
+
+class Command(BaseCommand):
+    help = "Import data from CSV to ModelItinerary Model"
+
+    def handle(self, *args, **options):
+        file_path = os.path.join(settings.BASE_DIR, 'ModelItinerary.csv')
+
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter='\t')
+            
+            for idx, row in enumerate(reader, start=1):
+                locations = [loc.strip() for loc in row[0].split(',') if loc.strip()]
+                spots = []
+                print(idx)
+
+                for idx, location in enumerate(locations):
+                    try:
+                        spot = Spot.objects.get(name=location)
+                    except Spot.DoesNotExist:
+                        print("Location not detected: ", location)
+    
+                # for location in locations:
+                #     print(location)
+                #     spots.append(spot)
+                    
+                # print(idx, spots)
+
+                # model_itinerary = ModelItinerary.objects.create(id=idx)
+                # model_itinerary.locations.set(spots)
