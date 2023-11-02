a799499a0a6ead66b87e42e3e0c05b31b0788c45
Arc-Data <arclight04@gmail.com>
2023-11-02 17:05:30 +0800

    reoptimized modal behavior

--- frontend/src/components/DateSettings.jsx
+++ frontend/src/components/DateSettings.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import Modal from "./Modal"
+import DatePicker from 'react-datepicker';
 
 const DateSettings = ({onClose}) => {
+    const [startDate, setStartDate] = useState()
+    const [endDate, setEndDate] = useState()
 
     return (
         <Modal onClose={onClose}>
-            <div>Something</div>
+            <div className="datesettings--modal">
+                <p class="datesettings--modal-title">Trip Date</p>
+                <form>
+                    <div className="datesettings--container">
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        selectsStart
+                        className="datesettings--form-input"
+                        startDate={startDate}
+                        endDate={endDate}
+                    />
+                    <DatePicker
+                        selected={endDate}
+                        onChange={(date) => setEndDate(date)}
+                        selectsEnd
+                        className="datesettings--form-input"
+
+                        startDate={startDate}
+                        endDate={endDate}
+                        minDate={startDate}
+                    />
+                    </div>
+                    <button className="plan--btn">Save</button>
+                </form>
+            </div>
         </Modal>
     )
 }
