ae038f71a74e844b2460b52458f65c2e4a3b637c
Arc-Data <arclight04@gmail.com>
2023-11-04 14:54:31 +0800

    removed unused location var in fetching itinerary data

--- frontend/src/pages/Plan.jsx
+++ frontend/src/pages/Plan.jsx
@@ -64,8 +64,6 @@ const Plan = () => {
 
 	useEffect(() => {
 		const fetchItineraryData = async (e) => {
-			const locations = []
-			
 			try {
 				const response = await fetch(`http://127.0.0.1:8000/api/plan/${id}/`, {
 					'method' : 'GET',
