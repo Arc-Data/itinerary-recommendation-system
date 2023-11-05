f913136d6540636b1d5007e5b9e14cf9c0a763d0
Arc-Data <arclight04@gmail.com>
2023-11-05 13:47:17 +0800

    display necessary recommendation item details

--- frontend/src/components/Recommendation.jsx
+++ frontend/src/components/Recommendation.jsx
@@ -1,7 +1,20 @@
+import { faLocation } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import getTimeDetails from "../utils/getTimeDetails"
+import getFeeDetails from "../utils/getFeeDetails"
+
 const Recommendation = ({recommendation}) => {
+    
     const displayItem = recommendation && recommendation.locations.map(item => {
         return (
-            <div key={item.id}>{item.name}</div>
+            <div key={item.id} className="assistant--recommendation-item">
+                <FontAwesomeIcon icon ={faLocation} />
+                <div>
+                    <p>{item.name}</p>
+                    <p>Open {getTimeDetails(item.schedule.opening)} - {getTimeDetails(item.schedule.closing)}</p>
+                    <p>Entrance Fee: {getFeeDetails(item.fee.min, item.fee.max)}</p>
+                </div>
+            </div>
         )
     })
     
@@ -10,6 +23,6 @@ const Recommendation = ({recommendation}) => {
             {displayItem}
         </div>
     )
-}
+} 
 
 export default Recommendation
