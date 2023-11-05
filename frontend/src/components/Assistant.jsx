69f7177f8484f28f4c434df2f7d803916483d954
Arc-Data <arclight04@gmail.com>
2023-11-05 10:56:58 +0800

    get user specific itinerary recommendations (content)

--- frontend/src/components/Assistant.jsx
+++ frontend/src/components/Assistant.jsx
@@ -1,9 +1,44 @@
 import dayjs from "dayjs"
+import { useContext, useEffect, useState } from "react"
+import AuthContext from "../context/AuthContext"
 
 const Assistant = ({onClose, day}) => {
+    const [loading, setLoading] = useState(true)
+    const [recommendations, setRecommendations] = useState([])
+    const { authTokens } = useContext(AuthContext)
+
     const formatDate = (day) => {
         return dayjs(day.date).format("MMM D YYYY, dddd")
     }
+
+    const getRecommendations = async (e) => {
+        setLoading(true)
+
+        try {
+            const response = await fetch(`http://127.0.0.1:8000/api/recommendations/content/`, {
+                'method' : 'GET',
+                'headers': {
+                    "Content-Type" : "application/json",
+                    "Authorization": `Bearer ${String(authTokens.access)}`, 
+                }
+            })
+
+            const data = await response.json()
+            console.log(data.recommendations)
+            setRecommendations(data.recommendations)
+        }
+        catch(error) {
+            console.log("An error occured")
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getRecommendations()
+        setLoading(false)
+    }, [])
 
     return (
         <>
@@ -15,12 +50,17 @@ const Assistant = ({onClose, day}) => {
                 </div>
                 <div className="assistant--content">
                     <p>Choose 1 from the list genereated. Note that this will replace the itinerary you created in <span className="assistant--date">{formatDate(day.date)}</span>.</p>
+                    { loading ? 
+                    <div>Loading Recommendations...</div>
+                    :
                     <div className="assistant--content-grid">
                         <div>1</div>
                         <div>2</div>
                         <div>3</div>
                     </div>
+                    }
                 </div>
+
                 <div className="assistant--footer">
                     <button className="assistant--btn btn-secondary" onClick={onClose}>Cancel</button>
                     <button className="assistant--btn btn-primary">Done</button>
