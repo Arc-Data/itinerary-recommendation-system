5d0e063f8af7a40f79b3cd3f4be986374576e7f2
Arc-Data <arclight04@gmail.com>
2023-11-09 14:15:37 +0800

    fixed itinerary list display on home page pt.2

--- frontend/src/components/Itinerary.jsx
+++ frontend/src/components/Itinerary.jsx
@@ -1,19 +1,30 @@
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faClose, faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 const Itinerary = ({itinerary}) => {
     return (
         <div className="itinerary">
-            <Link to ={`/plan/${itinerary.id}`}>
-                <img 
-                    src={`http://127.0.0.1:8000${itinerary.image}`} 
-                    height={240}
-                    width={240}
-                    className="itinerary--image"
-                    alt="" />
-            </Link>
+            <div className="itinerary--image-container">
+                <Link to ={`/plan/${itinerary.id}`}>
+                    <img 
+                        src={`http://127.0.0.1:8000${itinerary.image}`} 
+                        height={240}
+                        width={270}
+                        className="itinerary--image"
+                        alt="" />
+                </Link>
+                <div className="itinerary--settings">
+                    <div className="itinerary--delete">Delete</div>
+                    <FontAwesomeIcon icon={faClose} />
+                </div>
+            </div>
             <div>
-                <p className="itinerary--location-name">{itinerary.title}</p>
-                <p className="itinerary--location-date">{itinerary.trip_duration}</p>
+                <p className="itinerary--title">{itinerary.name}</p>
+                <div className="itinerary--details">
+                    <FontAwesomeIcon icon={faCalendar} />
+                    <p>{itinerary.trip_duration }</p>
+                </div>
             </div>
         </div>
     )
