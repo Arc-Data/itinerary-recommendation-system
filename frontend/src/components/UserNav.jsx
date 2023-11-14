2095abda61a2c9d47cd7a145b9093696e9497770
Arc-Data <arclight04@gmail.com>
2023-11-14 09:09:06 +0800

    changes to base navbar design

--- frontend/src/components/UserNav.jsx
+++ frontend/src/components/UserNav.jsx
@@ -27,37 +27,38 @@ const UserNav = () => {
 
     return (
         <nav className='user--navbar'>
-            <Link to = "/home">
-                <img className="cebu--route" src="../images/Untitled design.png" alt="LandingPage" />
-            </Link>
-
-            <form onSubmit={handleSearchSubmit} method="GET">
-                <div className='user--search'>
-                    <div className='search--bar-container'>
-                        <img className='search--icon' src={searchIcon} alt="Search Icon" />
-                        <input
-                            type="search"
-                            placeholder="Search CebuRoute"
-                            className='search--bar' 
-                            name="search" />
-                    </div>
-                </div>
-            </form>
-
-            <div className='user--links'>
-                <Link to="/create">
-                    <button className='user--create-trip'>Create a new trip</button>
+            <div className='nav-wrapper'>
+                <Link to = "/home">
+                    <img className="cebu--route" src="../images/Untitled design.png" alt="LandingPage" />
                 </Link>
-                <div className='user--profile' onClick={toggleDropdown}>
-                    <p>{letter}</p>
-                    {dropdown && 
-                    (
-                    <div className="user--dropdown-content">
-                        <button>View Itinerary</button>
-                        <button onClick={logoutUser}>Logout</button>
+                <form onSubmit={handleSearchSubmit} method="GET">
+                    <div className='user--search'>
+                        <div className='search--bar-container'>
+                            <img className='search--icon' src={searchIcon} alt="Search Icon" />
+                            <input
+                                type="search"
+                                placeholder="Search CebuRoute"
+                                className='search--bar' 
+                                name="search" />
+                        </div>
                     </div>
-                    )
-                    }
+                </form>
+
+                <div className='user--links'>
+                    <Link to="/create">
+                        <button className='user--create-trip'>Create a new trip</button>
+                    </Link>
+                    <div className='user--profile' onClick={toggleDropdown}>
+                        <p>{letter}</p>
+                        {dropdown && 
+                        <div className="user--dropdown-content">
+                            <Link to="/profile">
+                                Profile
+                            </Link>
+                            <button onClick={logoutUser}>Logout</button>
+                        </div>
+                        }
+                    </div>
                 </div>
             </div>
         </nav>
