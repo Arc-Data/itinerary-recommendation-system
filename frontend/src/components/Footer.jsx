fe75c2256a3727a2be93a9ebd893f2807be88c6b
Arc <arclight04@gmail.com>
2023-11-16 18:06:13 +0800

    renamed logo to fix instances where it goes undetected

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className='footer'>
           <div className="footer--content">
-            <img className='footer--logo' src="../images/Untitled design.png" />
+            <img className='footer--logo' src="/images/logo.png" />
             <p className='footer--text'>© 2023 CebuRoute. All rights reserved.</p>
             <ol className="icons">
               <li><img src="../images/instagram.png"  className="instagram" /></li>
